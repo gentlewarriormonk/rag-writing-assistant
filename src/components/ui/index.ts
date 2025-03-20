720269d26a03b17f428b8eebeb32ac6656c0b218
gentlewarriormonk <gareth.manning@gmail.com>
2025-03-20 19:21:42 +0100

    Update index.ts

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as LoadingSpinner } from './LoadingSpinner';
 export { default as ThinkingIndicator } from './ThinkingIndicator';
 export { default as GrokButton } from './GrokButton';
+export { default as ThemeToggle } from './ThemeToggle';
